c9bef2490356b34c79a770ff645ec5159d7e5fbb
Han <hanzz3884@naver.com>
2023-09-08 08:58:14 +0900

    09080858

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ const searchInputEl = searchEl.querySelector("input");
 
 const badgeEl = document.querySelector("header .badges");
 
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+
 searchEl.addEventListener("click", () => {
   searchInputEl.focus();
 });
@@ -23,6 +29,7 @@ window.addEventListener(
     console.log(window.scrollY);
     if (window.scrollY > 500) {
       // 배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션);
       gsap.to(badgeEl, 0.6, {
         opacity: 0,
         display: "none",
@@ -36,3 +43,46 @@ window.addEventListener(
     }
   }, 300)
 );
+
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7, // 0.7, 1.4, 2.1, 2.8
+    opacity: 1,
+  });
+});
+
+// new Swiper(선택자, 옵션);
+new Swiper(".notice-line .swiper", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper(".promotion .swiper", {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+promotionToggleBtn.addEventListener("click", () => {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add("hide");
+  } else {
+    // 보임 처리
+    promotionEl.classList.remove("hide");
+  }
+});
